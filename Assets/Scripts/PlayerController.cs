abb1beb8b611063a45aef46490fe473c44914f14
Irx99 <adriang.gtb@gmail.com>
2021-01-29 22:46:29 +0100

    mas cosas personajes y objetos destructibles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,8 +11,23 @@ public class PlayerController : MonoBehaviour
     public float velocityForward = 0.1f, velocityRight = 0.1f, mouseSensibility = 0.1f;
     public Camera playerCamera;
 
+    public Vector3 hitDetectionHalfCube = new Vector3(0.5f, 0.5f, 0.5f);
+    public float hitDetectionDistance = 3f;
+
     private float inputHorizontal, inputVertical;
     private Vector2 mousePosition;
+    private RaycastHit hit;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        playerInput.actions["mouseClick"].started += OnCustomMouseClick;
+    }
+
+    private void OnDestroy()
+    {
+        playerInput.actions["mouseClick"].started -= OnCustomMouseClick;
+    }
 
     private void Update()
     {
@@ -22,20 +38,28 @@ public class PlayerController : MonoBehaviour
 
         this.transform.Rotate(0, (mousePosition.x) * mouseSensibility, 0);
 
-        Debug.Log(Vector3.SignedAngle(playerCamera.transform.forward, this.transform.forward, this.transform.right));
-
         if(
             !(Vector3.SignedAngle(playerCamera.transform.forward, this.transform.forward, this.transform.right) > +60 && -mousePosition.y < 0) &&
             !(Vector3.SignedAngle(playerCamera.transform.forward, this.transform.forward, this.transform.right) < -60 && -mousePosition.y > 0)
         )
         {
             playerCamera.transform.Rotate((-mousePosition.y) * mouseSensibility, 0, 0);
-        }
-
+        }   
     }
 
     private void FixedUpdate()
     {
         characterController.SimpleMove(this.transform.forward * inputVertical * velocityForward + this.transform.right * inputHorizontal * velocityRight);
+    }
+
+    private void OnCustomMouseClick(InputAction.CallbackContext obj)
+    {
+        if(Physics.BoxCast(this.transform.position, hitDetectionHalfCube, this.transform.forward, out hit, this.transform.rotation, hitDetectionDistance))
+        {
+            if(hit.transform.GetComponent<DestructibleObject>())
+            {
+                hit.transform.GetComponent<DestructibleObject>().Destroy();
+            }
+        }
     }
 }
