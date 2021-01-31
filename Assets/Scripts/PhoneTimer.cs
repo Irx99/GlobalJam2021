d000513ebb8898a223381bf94585950585135eeb
vila6 <mrvila6@gmail.com>
2021-01-31 03:59:38 +0100

    timer

---
+++ Assets/Scripts/PhoneTimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class PhoneTimer : MonoBehaviour
+{
+    public Image timeBar;
+    public float levelTime;
+    public bool counting;
+    public AnimationCurve curvaDeTrucarElTiempo;
+    float timeRemaining;
+    float timeBarStartScale;
+    Vector3 aux;
+    private void Awake()
+    {
+        timeRemaining = levelTime;
+        timeBarStartScale = timeBar.rectTransform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeRemaining <= 0)
+        {
+            // TODO Game over
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
+        if (counting)
+        {
+            timeRemaining -= Time.deltaTime;
+
+            aux = timeBar.rectTransform.localScale;
+            aux.x = Mathf.Lerp(0, timeBarStartScale, curvaDeTrucarElTiempo.Evaluate(1 - timeRemaining / levelTime));
+            timeBar.color = Color.Lerp(Color.red, Color.white, curvaDeTrucarElTiempo.Evaluate(1- timeRemaining / levelTime));
+            timeBar.transform.localScale = aux;
+        }
+    }
+}
